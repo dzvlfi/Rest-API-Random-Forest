15543c5cb55cd98d203492bdbcabc7ea0f2f1a37
Dzulfiqar Ridha <dzulfi.ridha@gmail.com>
2019-09-23 22:53:08 +0700

    Update request.py

--- Code/request.py
+++ Code/request.py
@@ -1,7 +1,7 @@
 import requests
 # URL
-url = 'http://localhost:5000/api'
+url = 'http://dzvlfi.pythonanywhere.com/predicts'
 
 # Change the value of experience that you want to test
-r = requests.post(url,json={'exp':1.8,})
+r = requests.post(url,json=[{"LIMIT_BAL":30000, "EDUCATION":1, "SEX":1, "AGE":22, "PAY_1":1, "PAY_2":1, "PAY_3":1}])
 print(r.json())
